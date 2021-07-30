d8a293bd7a7e7a95e1da7eaf2ece0674cdf2a17b
Jean-Philippe Briend <jeanphilippe.briend@gmail.com>
2021-07-30 14:18:16 +0200

    Correct typo in tests

--- src/test/java/com/cloudbees/cd/plugin/sql/connections/HSQLDBConnection.java
+++ src/test/java/com/cloudbees/cd/plugin/sql/connections/HSQLDBConnection.java
@@ -1,7 +1,7 @@
 package com.cloudbees.cd.plugin.sql.connections;
 
 public class HSQLDBConnection extends SQLConnection {
-  private static final String jdbcUrlPattern = "jdbc:hsqldb:mem:databaseName]";
+  private static final String jdbcUrlPattern = "jdbc:hsqldb:mem:databaseName";
 
   public HSQLDBConnection(String server, String port, String databaseName, String properties, String login, String password) {
     super(server, port, databaseName, properties, jdbcUrlPattern, login, password);
